d906facbc5ae65af1315dbcf5f22e59b25b715bb
Orim Dominic <orimdominic@gmail.com>
2018-11-16 17:38:45 +0100

    style: Create base layout for fragrance data input

--- app/src/main/java/com/example/ws_kaizen/myfragrances/database/FragranceEntry.java
+++ app/src/main/java/com/example/ws_kaizen/myfragrances/database/FragranceEntry.java
@@ -2,12 +2,7 @@ package com.example.ws_kaizen.myfragrances.database;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.Ignore;
-import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
-
-import java.util.Date;
 
 @Entity(tableName = "fragrance")
 public class FragranceEntry {
@@ -15,26 +10,26 @@ public class FragranceEntry {
     @PrimaryKey(autoGenerate = true)
     private int id;
     private String name;
-    private int eu_price;   // end user price (retail price)
+    private int ret_price;   // end user price (retail price)
     private int ws_price;   // wholesale price
     @ColumnInfo(name = "quantity_in_stock")
     private int quantityInStock;
     private int gender; // 1 for male, 2 for female, 3 for unisex
 
     //  Used by Android
-    public FragranceEntry(String name, int eu_price, int ws_price, int quantityInStock, int gender) {
+    public FragranceEntry(String name, int ret_price, int ws_price, int quantityInStock, int gender) {
         this.name = name;
-        this.eu_price = eu_price;
+        this.ret_price = ret_price;
         this.ws_price = ws_price;
         this.quantityInStock = quantityInStock;
         this.gender = gender;
     }
 
     //  Used by Room
-    public FragranceEntry( int id, String name, int eu_price, int ws_price, int quantityInStock, int gender) {
+    public FragranceEntry(int id, String name, int ret_price, int ws_price, int quantityInStock, int gender) {
         this.id = id;
         this.name = name;
-        this.eu_price = eu_price;
+        this.ret_price = ret_price;
         this.ws_price = ws_price;
         this.quantityInStock = quantityInStock;
         this.gender = gender;
@@ -49,8 +44,8 @@ public class FragranceEntry {
         return name;
     }
 
-    public int getEu_price() {
-        return eu_price;
+    public int getRet_price() {
+        return ret_price;
     }
 
     public int getWs_price() {
@@ -73,8 +68,8 @@ public class FragranceEntry {
         this.name = name;
     }
 
-    public void setEu_price(int eu_price) {
-        this.eu_price = eu_price;
+    public void setRet_price(int ret_price) {
+        this.ret_price = ret_price;
     }
 
     public void setWs_price(int ws_price) {
