e2865842939292d25c6b806e86add5760da9f1a1
Orim Dominic <orimdominic@gmail.com>
2018-11-16 16:21:51 +0100

    chore: Refactor database config
    Refactor was necessary as price column could not
    be renamed, and also for total code overhaul

--- app/src/main/java/com/example/ws_kaizen/myfragrances/database/AppDatabase.java
+++ app/src/main/java/com/example/ws_kaizen/myfragrances/database/AppDatabase.java
@@ -1,19 +1,13 @@
 package com.example.ws_kaizen.myfragrances.database;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
-import android.arch.persistence.room.TypeConverters;
-import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 import android.util.Log;
 
-import static android.arch.persistence.room.ColumnInfo.TEXT;
-import static android.graphics.PorterDuff.Mode.ADD;
-
 @Database(entities = {FragranceEntry.class}, version = 3, exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase{
+public abstract class AppDatabase extends RoomDatabase {
 
     private static final String LOG_TAG = AppDatabase.class.getSimpleName();
     private static final Object LOCK = new Object();
@@ -26,30 +20,12 @@ public abstract class AppDatabase extends RoomDatabase{
                 Log.d(LOG_TAG, "Creating new database instance");
                 sInstance = Room.databaseBuilder(context.getApplicationContext(),
                         AppDatabase.class, AppDatabase.DATABASE_NAME)
-                        .addMigrations(FROM_1_TO_2, FROM_2_TO_3)
                         .build();
             }
         }
         Log.d(LOG_TAG, "Getting the database instance");
         return sInstance;
     }
-
-    static final Migration FROM_1_TO_2 = new Migration(1, 2) {
-//        Added 2  new columns to fragrance table: quantity_in_stock and quantity_sold
-        @Override
-        public void migrate(final SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE fragrance ADD COLUMN quantity_in_stock INTEGER NOT NULL DEFAULT 0 ");
-            database.execSQL("ALTER TABLE fragrance ADD COLUMN quantity_sold INTEGER NOT NULL DEFAULT 0 ");
-        }
-    };
-
-    static final Migration FROM_2_TO_3 = new Migration(2, 3) {
-//        Added gender column to fragrance table
-        @Override
-        public void migrate(final SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE fragrance ADD COLUMN gender INTEGER NOT NULL DEFAULT 1 ");
-        }
-    };
 
     public abstract FragranceDao mFragranceDao();
 
--- app/src/main/java/com/example/ws_kaizen/myfragrances/database/FragranceEntry.java
+++ app/src/main/java/com/example/ws_kaizen/myfragrances/database/FragranceEntry.java
@@ -13,98 +13,79 @@ import java.util.Date;
 public class FragranceEntry {
 
     @PrimaryKey(autoGenerate = true)
-    @NonNull
     private int id;
     private String name;
-    private int price;
+    private int eu_price;   // end user price (retail price)
+    private int ws_price;   // wholesale price
     @ColumnInfo(name = "quantity_in_stock")
     private int quantityInStock;
-    @ColumnInfo(name = "quantity_sold")
-    private int quantitySold;
     private int gender; // 1 for male, 2 for female, 3 for unisex
 
-
-    @Ignore
-    public FragranceEntry(String name, int price, int quantityInStock, int quantitySold, int gender) {
+    //  Used by Android
+    public FragranceEntry(String name, int eu_price, int ws_price, int quantityInStock, int gender) {
         this.name = name;
-        this.price = price;
+        this.eu_price = eu_price;
+        this.ws_price = ws_price;
         this.quantityInStock = quantityInStock;
-        this.quantitySold = quantitySold;
         this.gender = gender;
     }
 
-    public FragranceEntry(int id, String name, int price, int quantityInStock, int quantitySold, int gender) {
+    //  Used by Room
+    public FragranceEntry( int id, String name, int eu_price, int ws_price, int quantityInStock, int gender) {
         this.id = id;
         this.name = name;
-        this.price = price;
+        this.eu_price = eu_price;
+        this.ws_price = ws_price;
         this.quantityInStock = quantityInStock;
-        this.quantitySold = quantitySold;
         this.gender = gender;
-
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public int getId() {
         return id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getPrice() {
-        return price;
+    public int getEu_price() {
+        return eu_price;
+    }
+
+    public int getWs_price() {
+        return ws_price;
     }
 
     public int getQuantityInStock() {
         return quantityInStock;
     }
 
-    public void setQuantityInStock(int quantityInStock) {
-        this.quantityInStock = quantityInStock;
-    }
-
-    public int getQuantitySold() {
-        return quantitySold;
-    }
-
-    public void setQuantitySold(int quantitySold) {
-        this.quantitySold = quantitySold;
-    }
-
     public int getGender() {
         return gender;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEu_price(int eu_price) {
+        this.eu_price = eu_price;
+    }
+
+    public void setWs_price(int ws_price) {
+        this.ws_price = ws_price;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
     }
 
     public void setGender(int gender) {
         this.gender = gender;
     }
-
-    public String getGenderString() {
-        String genderString;
-        switch (gender) {
-            case 1:
-                genderString = "Male";
-                break;
-            case 2:
-                genderString = "Female";
-                break;
-            default:
-                genderString = "Unisex";
-                break;
-        }
-        return genderString;
-    }
-
 }
